f6016ac51b5e0deb1ab79c0d1ebf59217b3f9e43
Ashok IT <ashokitschool@gmail.com>
2025-12-09 10:36:13 +0530

    code updated

--- 26-Welcome-API/src/main/java/in/ashokit/rest/WelcomeRestController.java
+++ 26-Welcome-API/src/main/java/in/ashokit/rest/WelcomeRestController.java
@@ -1,7 +1,9 @@
 package in.ashokit.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,9 +13,15 @@ public class WelcomeRestController {
 
 	@Value("${msg}")
 	private String msg;
-	
+
+	@Autowired
+	private Environment env;
+
 	@GetMapping("/welcome")
 	public String getWelcomeMsg() {
-		return msg;		
+
+		String port = env.getProperty("server.port");
+
+		return msg + "(" + port + ")";
 	}
 }
